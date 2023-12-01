695cf5a9470b5781030727aadfec499cf30ef406
Ilya Kamyshov <kamyshov@gmail.com>
2023-12-01 11:47:01 +0300

    Доработано: учтены замечания преподавателя

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,16 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> tmp = new ArrayList<>(intList);
+        List<Integer> tmpList = new ArrayList<>();
 
-        for (int i = 0; i < tmp.size(); i++) {
-            if (tmp.get(i) <= 0 || tmp.get(i) % 2 != 0) {
-                tmp.remove(i);
-                i--;
+        for (Integer integer : intList) {
+            if (integer > 0 && integer % 2 == 0) {
+                tmpList.add(integer);
             }
         }
 
-        Collections.sort(tmp);
-        System.out.println(tmp);
-
+        Collections.sort(tmpList);
+        System.out.println(tmpList);
     }
 
 }
